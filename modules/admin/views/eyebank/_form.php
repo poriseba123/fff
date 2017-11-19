6bbff5efc12486add000c5f1268e96e62c7719f1
satyajit <satyajitghosh02@gmail.com>
2017-11-19 23:02:05 +0530

    map changes and css remove from view page

--- modules/admin/views/eyebank/_form.php
+++ modules/admin/views/eyebank/_form.php
@@ -25,120 +25,14 @@ use yii\helpers\ArrayHelper;
 <?php ActiveForm::end(); ?>
 
 </div>-->
-<style>
-    .main_contact_div .row{
-        margin-bottom: 5px;
-    }
-    .btn span.glyphicon {    			
-        opacity: 0;				
-    }
-    .btn.active span.glyphicon {				
-        opacity: 1;				
-    }
-    .radio-inline label{
-        margin-right:30px;
-    }
-    .time .row{
-        margin-bottom: 2px;
-    }
 
-</style>
-<style>
-    /* Always set the map height explicitly to define the size of the div
-     * element that contains the map. */
-    #map {
-        height: 100%;
-    }
-    /* Optional: Makes the sample page fill the window. */
-    html, body {
-        height: 100%;
-        margin: 0;
-        padding: 0;
-    }
-    #description {
-        font-family: Roboto;
-        font-size: 15px;
-        font-weight: 300;
-    }
-
-    #infowindow-content .title {
-        font-weight: bold;
-    }
-
-    #infowindow-content {
-        display: none;
-    }
-
-    #map #infowindow-content {
-        display: inline;
-    }
-
-    .pac-card {
-        margin: 10px 10px 0 0;
-        border-radius: 2px 0 0 2px;
-        box-sizing: border-box;
-        -moz-box-sizing: border-box;
-        outline: none;
-        box-shadow: 0 2px 6px rgba(0, 0, 0, 0.3);
-        background-color: #fff;
-        font-family: Roboto;
-    }
-
-    #pac-container {
-        padding-bottom: 12px;
-        margin-right: 12px;
-    }
-
-    .pac-controls {
-        display: inline-block;
-        padding: 5px 11px;
-    }
-
-    .pac-controls label {
-        font-family: Roboto;
-        font-size: 13px;
-        font-weight: 300;
-    }
-
-    #pac-input {
-        background-color: #fff;
-        font-family: Roboto;
-        font-size: 15px;
-        font-weight: 300;
-        margin-left: 12px;
-        padding: 0 11px 0 13px;
-        text-overflow: ellipsis;
-        width: 300px;
-    }
-
-    #pac-input:focus {
-        border-color: #4d90fe;
-    }
-
-    #title {
-        color: #fff;
-        background-color: #4d90fe;
-        font-size: 25px;
-        font-weight: 500;
-        padding: 6px 12px;
-    }
-    #target {
-        width: 345px;
-    }
-</style>
-<style>
-    .radio-inline label{
-        margin-right:30px;
-    }
-
-</style>
 <div class="portlet light bordered form-fit">
     <div class="portlet-title">
         <div class="caption">
             <i class="fa <?= $model->isNewRecord ? 'fa-plus' : 'fa-edit'; ?> font-green-haze" aria-hidden="true"></i>
 
             <span class="caption-subject font-green-haze bold uppercase">
-<?= Html::encode($this->title) ?>
+                <?= Html::encode($this->title) ?>
             </span>
         </div>
     </div>
@@ -146,7 +40,7 @@ use yii\helpers\ArrayHelper;
         <!-- BEGIN FORM-->
         <?php
         $form = ActiveForm::begin([
-                    'id'=>'create_eye_bank_form',
+                    'id' => 'create_eye_bank_form',
                     'options' => ['class' => 'form-horizontal form-row-seperated'],
                     'enableClientValidation' => false
                 ])
@@ -155,7 +49,7 @@ use yii\helpers\ArrayHelper;
             <div class="form-group">
                 <label class="control-label col-md-3">Name<span class="required">*</span></label>
                 <div class="col-md-6">
-<?= $form->field($model, 'name')->textInput(['class' => 'form-control'])->label(false); ?>
+                    <?= $form->field($model, 'name')->textInput(['class' => 'form-control'])->label(false); ?>
                 </div>
             </div>
         </div>
@@ -163,7 +57,7 @@ use yii\helpers\ArrayHelper;
             <div class="form-group">
                 <label class="control-label col-md-3">address<span class="required">*</span></label>
                 <div class="col-md-6">
-<?= $form->field($model, 'address')->textArea(['class' => 'form-control', 'rows' => '2'])->label(false); ?>
+                    <?= $form->field($model, 'address')->textArea(['class' => 'form-control', 'rows' => '2'])->label(false); ?>
                 </div>
             </div>
         </div>
@@ -217,6 +111,13 @@ use yii\helpers\ArrayHelper;
                     <input type="hidden" id="eyebankmaster-longitude" class="form-control" name="EyeBankMaster[longitude]">
                     <input id="pac-input" class="form-control controls1" type="text" placeholder="Search Box"><br>
                     <div id="map" style="height: 324px;width: 100%;"></div>
+                </div>
+                <div class="col-md-3">
+                    <div class="btn-group btn-group-solid">
+                        <button type="button" class="btn btn-success" style="font-size:17px;" onclick="getLocation();">
+                            MY LOCATION
+                        </button>
+                    </div>
                 </div>
             </div>
         </div>
@@ -276,7 +177,7 @@ use yii\helpers\ArrayHelper;
             <div class="form-group">
                 <label class="control-label col-md-3">Description<span class="required">*</span></label>
                 <div class="col-md-6">
-<?= $form->field($model, 'description')->textArea(['class' => 'form-control', 'rows' => '3'])->label(false); ?>
+                    <?= $form->field($model, 'description')->textArea(['class' => 'form-control', 'rows' => '3'])->label(false); ?>
                 </div>
             </div>
         </div>
@@ -286,19 +187,19 @@ use yii\helpers\ArrayHelper;
                 <div class="col-md-6">
                     <div class="main_contact_div">
                         <div>
-                        <div class="row row_0">
-                            <div class="col-md-8">
-                                <input type="text" class="form-control" name="contact_no[]" value="">
-                            </div>
-                            <div class="col-md-4">
-                                <div class="btn-group btn-group-solid">
-                                    <button type="button" class="btn btn-success" style="font-size:17px;" onclick="addPhone();">
-                                        + ADD MORE
-                                    </button>
+                            <div class="row row_0">
+                                <div class="col-md-8">
+                                    <input type="text" class="form-control" name="contact_no[]" value="">
+                                </div>
+                                <div class="col-md-4">
+                                    <div class="btn-group btn-group-solid">
+                                        <button type="button" class="btn btn-success" style="font-size:17px;" onclick="addPhone();">
+                                            + ADD MORE
+                                        </button>
+                                    </div>
                                 </div>
                             </div>
-                            </div>
-                         <div class="help-block"></div>
+                            <div class="help-block"></div>
                         </div>
 
                     </div>
@@ -307,31 +208,31 @@ use yii\helpers\ArrayHelper;
         </div>
 
 
-<?php if (!$model->isNewRecord) { ?>
+        <?php if (!$model->isNewRecord) { ?>
             <div class="form-group">
                 <label class="control-label col-md-3">Status <span class="required">*</span></label>
                 <div class="col-md-6">
                     <div class="radio-list">                        
                         <label class="radio-inline">
-    <?php
-    echo $form->field($model, 'status')->radioList(['1' => 'Active', '0' => 'Inactive'])->label(false);
-    ?>
+                            <?php
+                            echo $form->field($model, 'status')->radioList(['1' => 'Active', '0' => 'Inactive'])->label(false);
+                            ?>
                         </label>
                     </div>
                 </div>
             </div>
-<?php } ?>
+        <?php } ?>
 
 
         <div class="form-actions">
             <div class="row">
                 <div class="col-md-offset-3 col-md-6">
-<?= Html::submitButton($model->isNewRecord ? 'Create' : 'Update', ['class' => 'btn green']) ?>
+                    <?= Html::submitButton($model->isNewRecord ? 'Create' : 'Update', ['class' => 'btn green']) ?>
                     <a href="<?php echo Url::to(['eyebank/index']); ?>" class="btn default">Back</a>
                 </div>
             </div>
         </div>
-<?php ActiveForm::end() ?>
+        <?php ActiveForm::end() ?>
         <!-- END FORM-->
     </div>
 </div>
@@ -341,37 +242,98 @@ use yii\helpers\ArrayHelper;
             format: 'LT'
         });
     });
-     var global_val = 1;
-    function addPhone(){
-        $('.main_contact_div').append('<div><div class="row row_'+global_val+'">'+
-                            '<div class="col-md-8">'+
-                                '<input type="text" class="form-control" name="contact_no[]" value="">'+
-                            '</div>'+
-                            '<div class="col-md-4">'+
-                                '<div class="btn-group btn-group-solid">'+
-                                    '<button type="button" class="btn btn-danger" style="font-size:17px;" onclick="removeRow('+global_val+')">X</button>' +
-                                '</div>'+
-                            '</div>'+
-                            '</div>'+
-                            '<div class="help-block"></div>'+
-                        '</div>'
-                        );
-                global_val++;
+    var global_val = 1;
+    function addPhone() {
+        $('.main_contact_div').append('<div><div class="row row_' + global_val + '">' +
+                '<div class="col-md-8">' +
+                '<input type="text" class="form-control" name="contact_no[]" value="">' +
+                '</div>' +
+                '<div class="col-md-4">' +
+                '<div class="btn-group btn-group-solid">' +
+                '<button type="button" class="btn btn-danger" style="font-size:17px;" onclick="removeRow(' + global_val + ')">X</button>' +
+                '</div>' +
+                '</div>' +
+                '</div>' +
+                '<div class="help-block"></div>' +
+                '</div>'
+                );
+        global_val++;
     }
     function removeRow(id) {
         $('.row_' + id).remove();
     }
-</script>
-<script>
+    /////////////////////////////map script start/////////////////////////// 
+    currentlat = 20.5937;               //// india lat and long
+    currentlong = 78.9629;
+    message = false;
+    function geocodeLatLng(currentlat, currentlong) {
+        var latlng = {lat: parseFloat(currentlat), lng: parseFloat(currentlong)};
+        geocoder.geocode({'location': latlng}, function (results, status) {
+            if (status === 'OK') {
+                if (results[0]) {
+                    document.getElementById('pac-input').value = results[0].formatted_address;
+                    //var mylatlng = new google.maps.LatLng(currentlat, currentlong);
+                    // moveMarker(results[0].formatted_address, mylatlng, map);
+                    marker.setPosition(results[0].geometry.location);
+                    map.setCenter(results[0].geometry.location);
+                    map.setZoom(17);
+
+                } else {
+                    window.alert('No results found');
+                }
+            } else {
+                window.alert('Geocoder failed due to: ' + status);
+            }
+        });
+    }
+    function showPosition(position) {
+        currentlat = position.coords.latitude;
+        currentlong = position.coords.longitude;
+        document.getElementById('eyebankmaster-latitude').value = currentlat;
+        document.getElementById('eyebankmaster-longitude').value = currentlong;
+        geocodeLatLng(currentlat, currentlong);
+
+
+    }
+    function showError(error) {
+        switch (error.code) {
+            case error.PERMISSION_DENIED:
+                message = "User denied the request for Geolocation."
+                break;
+            case error.POSITION_UNAVAILABLE:
+                message = "Location information is unavailable."
+                break;
+            case error.TIMEOUT:
+                message = "The request to get user location timed out."
+                break;
+            case error.UNKNOWN_ERROR:
+                message = "An unknown error occurred."
+                break;
+        }
+    }
+    function getLocation() {
+        if (navigator.geolocation) {
+            //console.log(navigator.geolocation);
+            navigator.geolocation.getCurrentPosition(showPosition, showError);
+
+        } else {
+            message = "Geolocation is not supported by this browser.";
+        }
+        if (message) {
+            alert(message);
+        }
+    }
+
     function initAutocomplete() {
-        var myLatlng = new google.maps.LatLng(22, 79);
+        var myLatlng = new google.maps.LatLng(currentlat, currentlong);
         var myOptions = {
             zoom: 5,
             center: myLatlng,
+            scrollwheel: false,
             mapTypeId: google.maps.MapTypeId.ROADMAP
         }
-
-        var map = new google.maps.Map(document.getElementById("map"), myOptions),
+        geocoder = new google.maps.Geocoder;
+        map = new google.maps.Map(document.getElementById("map"), myOptions),
                 marker = new google.maps.Marker({
                     position: myLatlng,
                     map: map,
@@ -401,10 +363,12 @@ use yii\helpers\ArrayHelper;
             }
 
             moveMarker(place.name, place.geometry.location, map);
+            document.getElementById('eyebankmaster-latitude').value = place.geometry.location.lat();
+            document.getElementById('eyebankmaster-longitude').value = place.geometry.location.lng();
         });
     }
     function moveMarker(placeName, latlng, map) {
-        var marker = new google.maps.Marker({
+        marker = new google.maps.Marker({
             position: latlng,
             map: map,
             draggable: true
@@ -418,7 +382,8 @@ use yii\helpers\ArrayHelper;
         document.getElementById('eyebankmaster-latitude').value = event.latLng.lat();
         document.getElementById('eyebankmaster-longitude').value = event.latLng.lng();
     }
-       
+/////////////////////////////map script end/////////////////////////// 
 </script>
+
 
 
--- modules/admin/views/mortuary/_form_update.php
+++ modules/admin/views/mortuary/_form_update.php
@@ -25,113 +25,7 @@ use yii\helpers\ArrayHelper;
 <?php ActiveForm::end(); ?>
 
 </div>-->
-<style>
-    .main_contact_div .row{
-        margin-bottom: 5px;
-    }
-    .btn span.glyphicon {    			
-        opacity: 0;				
-    }
-    .btn.active span.glyphicon {				
-        opacity: 1;				
-    }
-    .radio-inline label{
-        margin-right:30px;
-    }
-    .time .row{
-        margin-bottom: 2px;
-    }
 
-</style>
-<style>
-    /* Always set the map height explicitly to define the size of the div
-     * element that contains the map. */
-    #map {
-        height: 100%;
-    }
-    /* Optional: Makes the sample page fill the window. */
-    html, body {
-        height: 100%;
-        margin: 0;
-        padding: 0;
-    }
-    #description {
-        font-family: Roboto;
-        font-size: 15px;
-        font-weight: 300;
-    }
-
-    #infowindow-content .title {
-        font-weight: bold;
-    }
-
-    #infowindow-content {
-        display: none;
-    }
-
-    #map #infowindow-content {
-        display: inline;
-    }
-
-    .pac-card {
-        margin: 10px 10px 0 0;
-        border-radius: 2px 0 0 2px;
-        box-sizing: border-box;
-        -moz-box-sizing: border-box;
-        outline: none;
-        box-shadow: 0 2px 6px rgba(0, 0, 0, 0.3);
-        background-color: #fff;
-        font-family: Roboto;
-    }
-
-    #pac-container {
-        padding-bottom: 12px;
-        margin-right: 12px;
-    }
-
-    .pac-controls {
-        display: inline-block;
-        padding: 5px 11px;
-    }
-
-    .pac-controls label {
-        font-family: Roboto;
-        font-size: 13px;
-        font-weight: 300;
-    }
-
-    #pac-input {
-        background-color: #fff;
-        font-family: Roboto;
-        font-size: 15px;
-        font-weight: 300;
-        margin-left: 12px;
-        padding: 0 11px 0 13px;
-        text-overflow: ellipsis;
-        width: 300px;
-    }
-
-    #pac-input:focus {
-        border-color: #4d90fe;
-    }
-
-    #title {
-        color: #fff;
-        background-color: #4d90fe;
-        font-size: 25px;
-        font-weight: 500;
-        padding: 6px 12px;
-    }
-    #target {
-        width: 345px;
-    }
-</style>
-<style>
-    .radio-inline label{
-        margin-right:30px;
-    }
-
-</style>
 <div class="portlet light bordered form-fit">
     <div class="portlet-title">
         <div class="caption">
@@ -379,7 +273,7 @@ use yii\helpers\ArrayHelper;
     function removeRow(id) {
         $('.row_' + id).remove();
     }
-        /////////////////////////////map script start/////////////////////////// 
+    /////////////////////////////map script start/////////////////////////// 
 <?php
 if ($model->isNewRecord) {
     ?>
