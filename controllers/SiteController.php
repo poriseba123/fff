d2ec89848ba1bb74d7e2e13a878b80f972d4fccd
satyajit <satyajitghosh02@gmail.com>
2018-02-10 23:18:28 +0530

    faq page and logo fixed

--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -422,7 +422,7 @@ class SiteController extends FrontendController {
     }
 
     public function actionCheckmailtemplate() {
-        $to = "satyajitghosh089@gmail.com";
+        $to = "satyajit.ghosh@cyber-swift.com";
         $body = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.";
         $content = Yii::$app->controller->renderPartial('@app/mail/layouts/template.php', array('message' => $body), true);
         echo "<pre>";
@@ -437,7 +437,7 @@ class SiteController extends FrontendController {
 //                ->send();
         echo "<pre>";
         echo "here</br/>";
-        //print_r($result);
+        print_r($result);
         echo "</pre>";
         exit;
     }
